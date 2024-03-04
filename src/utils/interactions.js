6f83609149056fa4228263c2d0c2099a5d800b90
galxeSaya <saya@galxe.com>
2024-03-04 11:46:10 +0800

    feat: init

--- src/utils/interactions.js
+++
@@ -1,8 +0,0 @@
-import { notification } from 'ant-design-vue';
-const [api, contextHolder] = notification.useNotification();
-export function openNotification(msg, description, type) {
-    api[type]({
-        message: msg,
-        description: description
-    })
-}
--- stop.sh
+++
@@ -1,4 +0,0 @@
-echo "Stopping container and removing image..."
-docker stop leg-july
-docker rm leg-july
-docker rmi leg-july:1.0.0
